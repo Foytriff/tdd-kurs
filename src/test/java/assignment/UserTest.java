1fc0bd216edcf375a7361c67b65e2c09ced0643a
Foytriff <gustavhf73@gmail.com>
2023-01-24 16:11:30 +0100

    anv kan skapas och fält för username och password finns (grön)

--- src/test/java/assignment/UserTest.java
+++ src/test/java/assignment/UserTest.java
@@ -3,29 +3,31 @@ package assignment;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class UserTest {
 
+
     @Test
-    public void user_class_exists(){
+    public void user_created(){
         User user;
-
-        User user = new User();
-
+        user = new User("anv", "pass");
         assertNotNull(user);
     }
 
-    @Test
-    @ValueSource(strings = {"username", "password"})
-    public void user_has_fields(String uname, String pword){
+    @ParameterizedTest
+    @CsvSource(value = {"anv, pass"})
+    public void user_has_fields(String username, String password){
 
         User user;
-"
-        user = new User("anv","pass");
 
-        assertEquals("username", user.getClass().getField(uname));
-        assertEquals("password", user.getClass().getField(pword));
+        user = new User(username, password);
+
+        assertEquals(username, user.getUsername());
+        assertEquals(password, user.getPassword());
+
+
     }
 }
